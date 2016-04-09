6260edab260ec6055acfbe8be5415ead6f258456
Mike Bernico <mike.bernico@gmail.com>
2016-04-09 10:48:25 -0500

    Initial Commit of package

---
+++ boruta/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='Boruta',
+      version='0.1',
+      description='Python Implementation of Boruta Feature Selection',
+      url='https://github.com/danielhomola/boruta_py',
+      author='Daniel Homola',
+      author_email='dani.homola@gmail.com',
+      license='BSD 3 clause',
+      packages=['boruta'],
+      zip_safe=False)
+
