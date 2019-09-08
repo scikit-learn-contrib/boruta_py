51a83e4b1c6f41313bf39108b92990bb867d0a77
Luca Massaron <lmassaron@users.noreply.github.com>
2019-09-08 14:15:07 +0200

    Update boruta_py.py
    final fix for random_state problem with XGBoost and LightGBM

--- boruta/boruta_py.py
+++ boruta/boruta_py.py
@@ -279,7 +279,7 @@ class BorutaPy(BaseEstimator, TransformerMixin):
                 self.estimator.set_params(n_estimators=n_tree)
 
             # make sure we start with a new tree in each iteration
-            #self.estimator.set_params(random_state=self.random_state)
+            self.estimator.set_params(random_state=self.random_state.randint(0, 10000))
 
             # add shadow attributes, shuffle them and train estimator, get imps
             cur_imp = self._add_shadows_get_imps(X, y, dec_reg)
